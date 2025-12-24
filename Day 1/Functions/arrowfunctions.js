2f335b72fabc78d4f7d67b036f118d45f5a2aeca
Barakaofficial <baraka38512@gmail.com>
2025-12-24 16:04:33 +0300

    Arrow functions

---
+++ Day 1/Functions/arrowfunctions.js
@@ -0,0 +1,26 @@
+// arrow fnction is the most shortest way to write functions
+
+// example-function to add 2 numbers
+
+// const add = (x,y) =>{
+//     return x + y;
+// };
+
+// console.log(add(3,4));
+
+
+// function to multply 3 intergers
+
+const product = (m,n,k) =>{
+    return m * n * k;
+};
+
+console.log(product(2,3,4));
+
+// function to print name and age
+
+const myDetail =(fname,age) =>{
+    return "My name is " + fname + ", "+ "and "+  "I'm " + age + " " + "years old";
+}
+
+console.log(myDetail("Baron",22));
